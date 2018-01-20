a31663788bd7ef8ebe555b2888cb6ca9b67da7d3
RoBo_ <mannvender001@gmail.com>
2018-01-21 01:48:58 +0530

    hash generator fixed
    sha512

--- routes/api/hash.js
+++ routes/api/hash.js
@@ -10,18 +10,28 @@ let email = 'test@gmail.com';
 let amount = 100;
 let txnID = 123;
 let udf1 = "";
-let hash = "";
+let hash;
 let phone = "8368604427";
+let value;
 
 let hashSequence = "";
 hashSequence += key + "|" + txnID + "|" + amount + "|" + productInfo + "|" + firstName + "|" + email + "|" + phone + "|" + udf1 + "|" + salt;
 console.log(hashSequence);
 
-hash = crypto.createHmac('sha512', hashSequence);
+
+// create hahs
+hash = crypto.createHmac('sha512', key);
+hash.update(hashSequence);
+value = hash.digest('hex');
+
+// print result
+console.log(value);
+
+// hash = crypto.createHmac('sha512', hashSequence);
 
 
 route.get('/', (req, res) => {
-    res.send({hash: hash})
+    res.send({hash: value})
 });
 
 exports = module.exports = route;
